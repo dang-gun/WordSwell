4b79495c63d60a0692a038912e3995df046b234b
dang-gun <c1dong8@gmail.com>
2023-08-24 10:57:47 +0900

    솔루션 구조도 생성, 툴 프로젝트 생성

---
+++ WordSwell.Tool.ApiModel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WordSwell.Tool.ApiModel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
