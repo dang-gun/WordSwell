3298fa403e7a421440b9de81ebfbde60959c3319
dang-gun <c1dong8@gmail.com>
2023-09-08 15:46:44 +0900

    마이그레이션 적용

---
+++ WordSwell.DB/Migrations/Mssql/20230908064510_FileDb_Edit001.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WordSwell.DB.Migrations.Mssql
+{
+    public partial class FileDb_Edit001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ErrorIs",
+                table: "FileDb",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<long>(
+                name: "idBoardPost",
+                table: "FileDb",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.UpdateData(
+                table: "BoardPost",
+                keyColumn: "idBoardPost",
+                keyValue: 1L,
+                column: "WriteTime",
+                value: new DateTime(2023, 9, 8, 15, 45, 9, 986, DateTimeKind.Local).AddTicks(7650));
+
+            migrationBuilder.UpdateData(
+                table: "BoardPost",
+                keyColumn: "idBoardPost",
+                keyValue: 2L,
+                column: "WriteTime",
+                value: new DateTime(2023, 9, 8, 15, 45, 9, 986, DateTimeKind.Local).AddTicks(7657));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErrorIs",
+                table: "FileDb");
+
+            migrationBuilder.DropColumn(
+                name: "idBoardPost",
+                table: "FileDb");
+
+            migrationBuilder.UpdateData(
+                table: "BoardPost",
+                keyColumn: "idBoardPost",
+                keyValue: 1L,
+                column: "WriteTime",
+                value: new DateTime(2023, 9, 7, 17, 31, 23, 388, DateTimeKind.Local).AddTicks(348));
+
+            migrationBuilder.UpdateData(
+                table: "BoardPost",
+                keyColumn: "idBoardPost",
+                keyValue: 2L,
+                column: "WriteTime",
+                value: new DateTime(2023, 9, 7, 17, 31, 23, 388, DateTimeKind.Local).AddTicks(379));
+        }
+    }
+}
